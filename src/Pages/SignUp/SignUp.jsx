42bf2bb0efec77bf8ccf550da39738b6cc2c7ea3
devpro-sajid <sfsajidforclash01@gmail.com>
2023-06-10 23:09:05 +0600

    footer complete

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -68,12 +68,35 @@ const SignUp = () => {
                         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
                             <input className="p-2 mt-6 rounded-md border" type="email" name="email" placeholder="Email"/>
                                 <div className="relative">
-                                    <input className="p-2 rounded-md border w-full" type="password" name="password" placeholder="Password"/>
+                                    <input className="p-2 rounded-md border w-full" type="password" name="password"  {...register("password", {
+                                    required: true,
+                                    minLength: 6,
+                                    maxLength: 20,
+                                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                                })} placeholder="Password"/>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
                                             <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
                                             <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
                                         </svg>
+                                        {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                                {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+                                {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>}
+                                {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase one lower case, one number and one special character.</p>}
                                 </div>
+                                <div className="relative">
+                                    <input className="p-2 rounded-md border w-full" type="password" {...register("confirmPassword", { required: true })} name="confirm-password" placeholder="Confirm Password"/>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
+                                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                                        </svg>
+                                        {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                                </div>
+                                <input type="text"  {...register("name", { required: true })} name="name" placeholder="Name" className="p-2 rounded-md border w-full" />
+                                {errors.name && <span className="text-red-600">Name is required</span>}
+
+                                <input type="text"  {...register("photoURL", { required: true })} placeholder="Photo URL" className="p-2 rounded-md border w-full" />
+                                {errors.photoURL && <span className="text-red-600">Photo URL is required</span>}
+
                                 <button className="bg-prime rounded-md text-white py-2 hover:scale-105 duration-300">Sign Up</button>
                         </form>
 
