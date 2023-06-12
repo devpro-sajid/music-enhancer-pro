1e84e0205afb8e7110d5a9a2595db4514b9adb4c
devpro-sajid <sfsajidforclash01@gmail.com>
2023-06-12 21:01:30 +0600

    student route done

--- src/Pages/Dashboard/Admin/ManageUsers.jsx
+++ src/Pages/Dashboard/Admin/ManageUsers.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useQuery } from "@tanstack/react-query";
-import useAxiosSecure from '../../../Hooks/useAxiosSecure';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import Swal from 'sweetalert2';
 
 const ManageUsers = () => {
