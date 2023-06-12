5456017a2e10634a61e93dc13d37eb061627c06a
devpro-sajid <sfsajidforclash01@gmail.com>
2023-06-12 20:32:47 +0600

    instructor route complete

--- src/Pages/Dashboard/Instructor/MyClasses.jsx
+++ src/Pages/Dashboard/Instructor/MyClasses.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../../hooks/useAuth';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
+
 
 const MyClasses = () => {
+    const {user}=useAuth();
+    const [axiosSecure] = useAxiosSecure();
+    const { data: classes = [], refetch } = useQuery(['classes'], async () => {
+        const res = await axiosSecure.get(`/classes?email=${user?.email}`)
+        return res.data;
+    })
     return (
         <div>
-            <h2>My Classes</h2>
+            <h2>My Classes{classes.length}</h2>
         </div>
     );
 };
